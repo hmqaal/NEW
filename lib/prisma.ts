bbb7c1e532ae9abc4b7ef75129acf88275515981
hmqaal <46423638+hmqaal@users.noreply.github.com>
2025-08-25 23:24:02 +0100

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,6 +2,10 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    // log: ["query"], // uncomment if you want to see SQL in dev
+  });
 
 if (process.env.NODE_ENV !== "production") (globalForPrisma as any).prisma = prisma;
